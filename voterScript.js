c3f7ceea94a82929b62e83c5ca854ec063c6d90b
Nik Molnar <nikmd23@gmail.com>
2013-07-19 19:10:28 -0400

    Added support for individual issue page

--- voterScript.js
+++ voterScript.js
@@ -19,20 +19,21 @@
         var pathPart = window.location.pathname.split('/'),
         owner = pathPart[1],
         repo = pathPart[2],
-        cacheDuration = -500, // in milliseconds
+        issue = pathPart[4],
+        cacheDuration = 3600000, // in milliseconds
         up = new RegExp(/:\+1:|:thumbsup:/),
         down = new RegExp(/:-1:|:thumbsdown:/); // emoji options can be customized here
        
         $('.list-group-item-meta').each(function(index){
             var that = $(this),
             id = that.parent().attr('id').replace('issue_',''),
-            uri = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + id + '/comments',
-            itemCache = JSON.parse(window.localStorage.getItem(key(owner, repo, id)));
+            itemCache = getCacheItem(owner, repo, id, cacheDuration);
           
-            if (itemCache !== null && itemCache.time > Date.now() - cacheDuration){
+            if (itemCache !== null){
                 updateUi(that, itemCache.votes);
             } else {
-                var loader = displayLoadMessage(that);
+                var loader = displayLoadMessage(that),
+                uri = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + id + '/comments';
                 $.ajax({url: uri, context: that, dataType: 'json', global: false, headers: {Authorization: 'token 04f12d48ff23766940f8d9745e8d6918ee29399b'}})
                 .done(function(data) {
                     var votes = 0;
@@ -50,6 +51,27 @@
                 });
             }
         });
+        
+        if(issue !== undefined) {
+            var uri = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issue + '/comments',
+            upvotes = 0, downvotes = 0;
+            $.ajax({url: uri, dataType: 'json', global: false, headers: {Authorization: 'token 04f12d48ff23766940f8d9745e8d6918ee29399b'}})
+            .done(function(data) {
+                $.each(data, function(index, value){
+                    upvotes += up.test(value.body);
+                    downvotes -= down.test(value.body);
+                });
+                window.localStorage.setItem(key(owner, repo, issue), JSON.stringify({time: Date.now(), votes: upvotes - downvotes}));
+                var participation = $('.pull-participation'),
+                upUi = $('<p class="quickstat"><strong>' + upvotes + '</strong> up votes <img width="20" align="absmiddle" height="20" class="emoji" src="https://github.global.ssl.fastly.net/images/icons/emoji/+1.png" style="vertical-align: text-bottom;"></p>'),
+                downUi = $('<p class="quickstat"><strong>'+ Math.abs(downvotes)+'</strong> down votes <img width="20" align="absmiddle" height="20" class="emoji" src="https://github.global.ssl.fastly.net/images/icons/emoji/-1.png" style="vertical-align: text-top;"></p>');
+                
+                participation.append(upUi).append(downUi);
+            })
+            .fail(function(){
+                console.log('there was an error :(');
+            });
+        }
     }
     
     function updateUi(issueDom, votes){
@@ -70,5 +92,15 @@
     
     function key(owner, repo, id){
         return owner + '-' + repo + '-' + id + '-github-votes';
+    }
+    
+    function getCacheItem(owner, repo, id, cacheDuration){
+        var result = JSON.parse(window.localStorage.getItem(key(owner, repo, id)));
+        
+        if (result === null){
+            return null;
+        }
+        
+        return result.time > Date.now() - cacheDuration ? result : null;
     }
 })(jQuery);
